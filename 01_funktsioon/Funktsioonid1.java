ad6c45e3a98ead59884bef5aa26154776778b0ec
Johannes Theodor Kollist <31847434+johanneskollist@users.noreply.github.com>
2022-02-07 14:35:26 +0200

    Create Funktsioonid1.java

---
+++ 01_funktsioon/Funktsioonid1.java
@@ -0,0 +1,12 @@
+public class Funktsioonid1{
+    public static double tollideks(double sentimeeter){
+        return sentimeeter*0.39370;
+    }
+    //tehke funktsiooniga sentimeetrid
+    public static void main(String[] arg){
+        if(arg.length>0){
+            double t=Double.parseDouble(arg[0]);
+            System.out.println(tollideks(t));
+        }
+    }
+}
