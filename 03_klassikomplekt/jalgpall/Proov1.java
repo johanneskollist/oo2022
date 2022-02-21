7f773bbb4f5c465cf7372ed7d1e649d4da7504b7
Johannes Kollist <johanneskollist@users.noreply.github.com>
2022-02-21 16:10:32 +0200

    kolmas tund

---
+++ 03_klassikomplekt/jalgpall/Proov1.java
@@ -0,0 +1,9 @@
+public class Proov1{
+	public static void main(String[] arg){
+		Mangija m1=new Mangija("Kollist", 7, 12, 30, 3.5);
+		System.out.println(m1);
+		System.out.println(m1.kaugus(10, 12));
+		m1.liigu(1);
+		System.out.println(m1);
+	}
+}
---
+++ 03_klassikomplekt/jalgpall/Proov2.java
@@ -0,0 +1,22 @@
+public class Proov2{
+        public static void main(String[] arg){
+                Mangija m1=new Mangija("Kollist", 7, 12, 30, 3.5);
+		Mangija m2=new Mangija("Tauts", 17, 12, 150, 4.5);
+		Meeskond punased=new Meeskond("FA Kalev");
+		punased.lisaMangija(m1);
+		punased.lisaMangija(m2); 
+                System.out.println(punased);
+		punased.liigu(3);
+		System.out.println(punased);
+		System.out.println(punased.lahim(5, 20));
+        }
+}                                                                                                                                                            
+                                                                               
+                                                                               
+                                                                               
+                                                                               
+                                                                               
+                                                                               
+                                                                                    
+                                                                               
+                                   
